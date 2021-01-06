2ba7b301ca7ccfdce10259f728ece9de5f3d16c5
Elvis <okaforelvis24@gmail.com>
2021-01-05 16:22:08 -0800

    robofriends app

---
+++ src/Scroll.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Scroll = (props) => {
+	return <div style={{ overflow: 'scroll', border: '1px', height: '500px' }}>{props.children}</div>;
+};
+
+export default Scroll;
